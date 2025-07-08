276949f970774e4a76a4b581bedde0efdcd2364a
Anton <nokker@gmail.com>
2025-07-08 15:42:06 +0900

    fix migrations error

--- apps/frontend/drizzle.config.ts
+++ apps/frontend/drizzle.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema",
   dialect: "postgresql",
+
   dbCredentials: {
     url: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
   },
