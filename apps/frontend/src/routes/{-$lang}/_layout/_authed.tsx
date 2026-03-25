06fb478290df2074ff8691611f32691c9cf72204
Anton Zhuravlev <nokker@gmail.com>
2026-03-25 18:36:22 +0900

    feat: remove /admin/datasets route and sidebar nav link

--- apps/frontend/src/routes/{-$lang}/_layout/_authed.tsx
+++ apps/frontend/src/routes/{-$lang}/_layout/_authed.tsx
@@ -6,7 +6,6 @@ import {
 } from "@tanstack/react-router";
 import {
   Cuboid,
-  Database,
   Files,
   LibraryBig,
   Newspaper,
@@ -26,7 +25,6 @@ export const tabParamSchema = z.enum([
   "content",
   "users",
   "researches",
-  "datasets",
   "assets",
 ]);
 
@@ -197,17 +195,6 @@ function NavPanel() {
         }
         tab="researches"
       />
-      {canViewCms && (
-        <PanelItem
-          title={
-            <span>
-              <Database className="mr-2 inline size-5 align-middle leading-normal" />
-              Datasets
-            </span>
-          }
-          tab="datasets"
-        />
-      )}
       {canViewCms && (
         <PanelItem
           title={
