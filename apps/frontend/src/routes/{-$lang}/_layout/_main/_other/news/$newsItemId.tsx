5aa980fb11ae0310e9185fd599d0d65cbf8eb985
Anton Zhuravlev <nokker@gmail.com>
2026-04-08 10:46:21 +0900

    fix scroll

--- apps/frontend/src/routes/{-$lang}/_layout/_main/_other/news/$newsItemId.tsx
+++ apps/frontend/src/routes/{-$lang}/_layout/_main/_other/news/$newsItemId.tsx
@@ -1,10 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { MarkdownWithTOC } from "@/components/MarkdownWithTOC";
-import { getNewsTranslationQueryOptions } from "@/serverFunctions/news";
-import { renderMarkdown } from "@/utils/markdown";
 import { Card } from "@/components/Card";
 import { Markdown } from "@/components/Merkdown";
+import { getNewsTranslationQueryOptions } from "@/serverFunctions/news";
+import { renderMarkdown } from "@/utils/markdown";
 
 export const Route = createFileRoute(
   "/{-$lang}/_layout/_main/_other/news/$newsItemId",
@@ -34,15 +33,13 @@ function RouteComponent() {
   const { contentHtml, publishedAt, title } = Route.useLoaderData();
 
   return (
-    <Card className="w-full py-6">
-      <div className="prose mx-auto prose-a:text-secondary-light prose-a:visited:text-secondary-lighter flex justify-center gap-5 prose-h1:text-secondary prose-h1:font-medium prose-h1:mt-8 prose-h1:mb-16">
-        <div className="flex-1">
-          <h1>
-            {title}
-            <p className="text-xs text-foreground-light">{publishedAt}</p>
-          </h1>
-          <Markdown contentHtml={contentHtml} />
-        </div>
+    <Card className="min-w-0 flex-1">
+      <div className="prose mx-auto prose-a:text-secondary-light prose-a:visited:text-secondary-lighter prose-h1:mt-8 prose-h1:mb-16 prose-h1:font-medium prose-h1:text-secondary">
+        <h1>
+          {title}
+          <p className="text-xs text-foreground-light">{publishedAt}</p>
+        </h1>
+        <Markdown contentHtml={contentHtml} />
       </div>
     </Card>
   );
--- apps/frontend/src/routes/{-$lang}/_layout/_main/_other/news/route.tsx
+++ apps/frontend/src/routes/{-$lang}/_layout/_main/_other/news/route.tsx
@@ -1,9 +1,26 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useParams } from "@tanstack/react-router";
+
+import { newsPublicSearchParamsSchema } from "@/utils/queryParams";
+import { NewsList } from "./-NewsList";
 
 export const Route = createFileRoute("/{-$lang}/_layout/_main/_other/news")({
+  validateSearch: newsPublicSearchParamsSchema,
   loader({ context }) {
     return {
       crumb: context.messages?.Navbar?.["all-news"],
     };
   },
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  const params = useParams({ strict: false });
+  const selectedNewsItemId = params.newsItemId;
+
+  return (
+    <div className="flex items-start gap-3">
+      <NewsList selectedNewsItemId={selectedNewsItemId} />
+      <Outlet />
+    </div>
+  );
+}
