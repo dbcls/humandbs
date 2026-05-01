98baf4dd6d809cab0803db6b28a4f2c183b06bf6
永野 朗夫 <nagano@me.com>
2026-05-01 17:21:33 +0900

    style: adjust layout and spacing for top page

--- apps/frontend/src/routes/{-$lang}/_layout/_main/_home.tsx
+++ apps/frontend/src/routes/{-$lang}/_layout/_main/_home.tsx
@@ -31,35 +31,37 @@ function RouteComponent() {
 
   return (
     // All that after the Navbar component
-    <section className="main-content flex flex-col items-stretch gap-8">
-      <section className="flex h-fit items-start justify-between gap-8">
-        <div className="prose-h1:text-secondary prose-h1:text-lg prose-h1:mt-8 prose-h1:mb-16 flex flex-1 flex-col items-center">
-          <Outlet />
-          <div className="mt-8 flex flex-wrap justify-center gap-4 [&_button>svg]:ml-4">
-            <Button
-              variant={"accent"}
-              onClick={() => {
-                navigate({ to: "/{-$lang}/data-submission" });
-              }}
-              size={"xl"}
-              className="relative block h-[6.8rem] w-[27rem] text-center"
-            >
-              <span>{t("data-submission-button")}</span>
+    <section className="w-full flex flex-col items-stretch gap-8">
+      <section className="flex h-fit items-start justify-between gap-4">
+        <div className="prose-h1:text-secondary prose-h1:text-lg prose-h1:mt-8 prose-h1:mb-16 flex flex-1 flex-col items-center rounded-md bg-white p-8 pb-12">
+          <div className="w-full max-w-5xl flex flex-col items-center">
+            <Outlet />
+            <div className="mt-8 flex flex-wrap justify-center gap-4 [&_button>svg]:ml-4">
+              <Button
+                variant={"accent"}
+                onClick={() => {
+                  navigate({ to: "/{-$lang}/data-submission" });
+                }}
+                size={"xl"}
+                className="relative block h-[6.8rem] w-[27rem] text-center"
+              >
+                <span>{t("data-submission-button")}</span>
 
-              <SubmitDataIcon className="absolute right-2 bottom-2 h-auto w-40" />
-            </Button>
+                <SubmitDataIcon className="absolute right-2 bottom-2 h-auto w-40" />
+              </Button>
 
-            <Button
-              variant={"action"}
-              size={"xl"}
-              className="relative block h-[6.8rem] w-[27rem] text-center"
-              onClick={() => {
-                navigate({ to: "/{-$lang}/data-use" });
-              }}
-            >
-              <span>{t("data-usage-button")}</span>
-              <UseDataIcon className="absolute bottom-2 left-0 h-auto w-40" />
-            </Button>
+              <Button
+                variant={"action"}
+                size={"xl"}
+                className="relative block h-[6.8rem] w-[27rem] text-center"
+                onClick={() => {
+                  navigate({ to: "/{-$lang}/data-use" });
+                }}
+              >
+                <span>{t("data-usage-button")}</span>
+                <UseDataIcon className="absolute bottom-2 left-0 h-auto w-40" />
+              </Button>
+            </div>
           </div>
         </div>
 
