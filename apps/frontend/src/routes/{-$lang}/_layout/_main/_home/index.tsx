fdcabcb21d5cf4c87b7b6bb61c281b1eef1ed3c9
Lucas <feriau.lucas@gmail.com>
2026-05-01 17:15:57 +0900

    Fix searchbar style

--- apps/frontend/src/routes/{-$lang}/_layout/_main/_home/index.tsx
+++ apps/frontend/src/routes/{-$lang}/_layout/_main/_home/index.tsx
@@ -1,8 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Search } from "lucide-react";
-import { useState } from "react";
 
-import { Input } from "@/components/Input";
 import { Markdown } from "@/components/Merkdown";
 import { $getLatestPublishedDocumentVersion } from "@/serverFunctions/documentVersion";
 import { renderMarkdown } from "@/utils/markdown";
@@ -22,31 +19,9 @@ export const Route = createFileRoute("/{-$lang}/_layout/_main/_home/")({
 
 function RouteComponent() {
   const { contentHtml, title } = Route.useLoaderData();
-  const navigate = Route.useNavigate();
-  const [query, setQuery] = useState("");
-
   return (
     <>
       <h1>{title}</h1>
-
-      <Input
-        type="text"
-        className="mb-4 w-full max-w-4xl"
-        value={query}
-        beforeIcon={<Search size={18} />}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" && query.trim()) {
-            navigate({
-              to: "/{-$lang}/dataset",
-              search: { query: query.trim() },
-            });
-          }
-        }}
-      />
-
       <Markdown contentHtml={contentHtml} />
     </>
   );
